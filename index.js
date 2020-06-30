8e3befff6791dfbc1718e83b8e519f5a61810776
James Dandy <intensetoxic@gmail.com>
2020-06-30 18:53:34 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,3 +1,3 @@
-console.error("Error")
-console.warn("Warning")
-console.log("Log")
+console.error("A Critical Error Has Occured!")
+console.warn("A Dire Warning Has Occured!")
+console.log("A Simple Logging Event Has Occured!")
